14f17fb97a3c4c0f2c60dc915c427f9541d99815
Perla <psmaniotto@aconcaguasf.com>
2022-01-27 12:07:19 -0300

    fix lang component, remove flags

--- src/components/Lang.jsx
+++ src/components/Lang.jsx
@@ -7,11 +7,11 @@ import { Event } from '../util/Event';
 import TXT from '../lang/header.json';
 
 const LANG = [
-  { lang: 'es', flag: 'esp' }, 
-  { lang: 'en', flag: 'gbr' }, 
-  { lang: 'fr', flag: 'fra' }, 
-  { lang: 'br', flag: 'bra' }, 
-  { lang: 'de', flag: 'deu' }
+  { lang: 'es', label: 'Español' }, 
+  { lang: 'en', label: 'English' }, 
+  { lang: 'fr', label: 'Français' }, 
+  { lang: 'br', label: 'Português' }, 
+  { lang: 'de', label: 'Deutsch' }
 ]
 
 class Lang extends Component {
@@ -44,17 +44,15 @@ class Lang extends Component {
           <span className="col-sm">{ TXT.lang[this.state.lang] }</span>
           <div className="col-sm-auto" role="group" aria-label="Languages">
             <div className="row">
-            {LANG.map(({ lang, flag },k)=> <div className="col-auto" key={k}>
+            {LANG.map(({ lang, label },k)=> <div className="col-auto" key={k}>
                 <input type="radio" className="lang-input" name="lang"
                   id={"lang1"+lang} autoComplete="off" value={lang}
                   onChange={this.changeLang} checked={Boolean(this.state.lang==lang)}
                 />
                 <label className="lang-lbl" htmlFor={"lang1"+lang}>
-                  <img
-                    className="img-thumbnail fs-2 lang-flag"
-                    src={`https://restcountries.eu/data/${flag}.svg`}
-                    alt={lang}
-                  />
+                  <span className="btn badge badge-light text-primary" title={label}>
+                    {lang.toUpperCase()}
+                  </span>
                 </label>
               </div>
             )}
